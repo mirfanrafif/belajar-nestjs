56476010d014009b92a1efe9aef9075fe52eb839
mirfanrafif17 <mirfanrafif17@gmail.com>
2022-03-02 12:23:19 +0700

    swagger

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class UserDto {
   @IsNotEmpty()
+  @ApiProperty()
   nama: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   nohp: string;
 }
